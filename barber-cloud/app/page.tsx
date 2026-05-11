2110aca38e3ae0e040d78586e0dac44d13415a16
kaiocotrim <kaioalan40@gmail.com>
2026-05-11 12:00:33 -0300

    feat: card data hours

--- barber-cloud/app/page.tsx
+++ barber-cloud/app/page.tsx
@@ -4,6 +4,9 @@ import { text } from "stream/consumers";
 import Header from "./_components/header";  
 import { Input } from "./_components/ui/input";
 import { Search, SearchIcon } from "lucide-react";
+import { Card, CardContent } from "./_components/ui/card";
+import { Badge } from "./_components/ui/badge";
+import { Avatar, AvatarGroupCount, AvatarImage } from "./_components/ui/avatar";
 
 
 
@@ -16,7 +19,7 @@ export default function Home() {
         <h2 className="text-xl font-bold">Olá, Kaio!</h2>
         <p>Segunda-feira, 12 de junho</p>
 
-
+        {/* Search Bar */}
         <div className="flex items-center gap-2 mt-6">
           <Input placeholder="Pesquisar..." className="h-10" />
           <Button size="icon" variant="outline" className="h-10 w-10">
@@ -24,11 +27,35 @@ export default function Home() {
           </Button>
         </div>
         
+        {/* Banner Image */}
         <div className="relative w-full mt-6 h-[150px] rounded-xl">
           <Image src="/Banner-01.png" alt="Banner-barberCloud" fill className="object-cover" />
         </div>
       </div>
-      
+
+
+      {/* Agendamento section */}
+      <Card className="mt-6">
+        <CardContent className="flex justify-between p-0 ">
+        {/* DIV esquerda  */}
+        <div className="flex flex-col gap-2 py-5 pl-5 ">
+          <Badge variant="outline" className="bg-blue-800">Confirmado</Badge>
+          <h3>Corte de cabelo</h3>
+          <div className="flex items-center gap-2">
+            <Avatar className="w-6 h-6">
+              <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" alt="Avatar do cliente" />
+            </Avatar>
+            <p className="text-sm">Las Vegas Barbearia</p>
+          </div>
+        </div>
+        {/* DIV direita  */}
+        <div className="flex flex-col items-center justify-center px-4 border-l-2 border-solid px-5">
+        <p className="text-sm">Maio</p>
+        <p className="text-2xl">12</p>
+        <p className="text-sm">16:00</p>
+        </div>
+        </CardContent>
+      </Card>
     </div>
   );
 }
