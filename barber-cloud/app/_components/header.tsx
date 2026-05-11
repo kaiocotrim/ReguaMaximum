e9a6614dccc4fc35a8e619f4c47ecd49762ccfdd
kaiocotrim <kaioalan40@gmail.com>
2026-05-11 11:11:30 -0300

    chore: add home page

---
+++ barber-cloud/app/_components/header.tsx
@@ -0,0 +1,19 @@
+import { MenuIcon } from "lucide-react";
+import { Button } from "./ui/button";
+import { Card, CardContent } from "./ui/card";
+import Image from "next/image";
+
+const Header = () => {
+    return (  
+        <Card className="rounded-none ">
+            <CardContent className="flex flex-row items-center justify-between">
+                <Image src="/logoBarber1.png" alt="BarberCloud Logo" width={150} height={50} />
+                     <Button size="icon" variant="outline">
+                <MenuIcon />
+            </Button>
+            </CardContent>
+        </Card>
+    );
+}
+ 
+export default Header;
--- barber-cloud/app/page.tsx
+++ barber-cloud/app/page.tsx
@@ -1,14 +1,34 @@
 import Image from "next/image";
 import { Button } from "@/app/_components/ui/button";
 import { text } from "stream/consumers";
+import Header from "./_components/header";  
+import { Input } from "./_components/ui/input";
+import { Search, SearchIcon } from "lucide-react";
+
 
 
 
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to BarberCloud</h1>
-            <Button>Get Started</Button>
+      <Header />
+      <div className="p-6">
+        <h2 className="text-xl font-bold">Olá, Kaio!</h2>
+        <p>Segunda-feira, 12 de junho</p>
+
+
+        <div className="flex items-center gap-2 mt-6">
+          <Input placeholder="Pesquisar..." className="h-10" />
+          <Button size="icon" variant="outline" className="h-10 w-10">
+            <SearchIcon />
+          </Button>
+        </div>
+        
+        <div className="relative w-full mt-6 h-[150px] rounded-xl">
+          <Image src="/Banner-01.png" alt="Banner-barberCloud" fill className="object-cover" />
+        </div>
+      </div>
+      
     </div>
   );
 }
