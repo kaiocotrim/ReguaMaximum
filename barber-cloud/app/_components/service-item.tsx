d6c8c51ae394404ae54aa2ee4c7a4b8cd821602a
kaiocotrim <kaioalan40@gmail.com>
2026-05-15 11:54:18 -0300

    feat: 1/5 color

--- barber-cloud/app/_components/service-item.tsx
+++ barber-cloud/app/_components/service-item.tsx
@@ -1,11 +1,30 @@
 import {BarbeshopService} from "@prisma/client";   
+import Image from "next/image";
+import { Button } from "./ui/button";
 
 interface ServiceItemProps {
   service: BarbeshopService;
 }
 
 const ServiceItem = ({ service }: ServiceItemProps) => {
-    return ( <h1>{service.name}</h1> );
+    return (
+        <div className="flex items-center gap-3 p-3 border rounded-lg mb-3">
+            {/* Exibir imagem do serviço */}
+            <div className="relative max-h-[110px] max-w-[110px] min-h-[110px] min-w-[110px] rounded-lg overflow-hidden">
+                <Image src={service.imageUrl} alt={service.name} fill className="object-cover" />
+            </div>
+            {/* Direita do item: nome e preço */}
+            <div className="space-y-3">
+                <h3 className="text-lg font-semibold text-[#C3F32C]">{service.name}</h3>
+                <p className="text-sm text-gray-500">{service.description}</p>
+                {/* Exibir preço do serviço */}
+                <div className="flex items-center justify-between">
+                    <p className="text-xl font-bold">R$ {service.price.toFixed(2)}</p>
+                    <Button size="sm" variant="outline" className="ml-auto">Agendar</Button>
+                </div>
+            </div>
+        </div>
+     );
 }
  
 export default ServiceItem;
--- barber-cloud/app/barbershops/[id]/page.tsx
+++ barber-cloud/app/barbershops/[id]/page.tsx
@@ -5,15 +5,13 @@ import { ChevronLeft, MapIcon, MenuIcon, StarIcon } from "lucide-react"
 import Link from "next/link"
 import { Card } from "@/app/_components/ui/card"
 import ServiceItem from "@/app/_components/service-item"
-
+import { User } from "lucide-react"
 
 interface BarbershopPageProps {
   params: {
     id: string
-  };
-
+  }
 }
-
 
 const BarbershopPage = async ({ params }: BarbershopPageProps) => {
   const { id } = await params
@@ -24,16 +22,14 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
     include: {
       services: true,
     },
-
   })
-
 
   if (!barbershop) return <p>Barbearia não encontrada.</p>
 
   console.log("Barbearia encontrada:", barbershop.services)
 
   return (
-    <div>
+    <div >
       {/* Exibir img da barbearia */}
       <div className="relative h-[250px] w-full">
         <Image
@@ -53,55 +49,68 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
           </Link>
         </Button>
 
-        
         <Button
           size="icon"
           variant="secondary"
-          className="absolute top-4 right-4  bg-black"
+          className="absolute top-4 right-4 bg-black"
         >
           <Link href="/">
             <MenuIcon className="h-5 w-5 text-[#C3F32C]" />
           </Link>
         </Button>
-
-
       </div>
 
-             {/* <h2 className="text-xl font-bold">
+      {/* <h2 className="text-xl font-bold">
             Olá, <span className="shine-text">Bruno Odorissi Campaner.</span>
           </h2> */}
 
-      {/* Exibir nome da barbearia */}      
-      <div className="relative p-5 pb-0">
-        <h1 className="mb-3 text-2xl font-bold text-[#C3F32C]"><span className="shine-text">{barbershop.name}</span> </h1>
+      {/* Exibir nome da barbearia */}
+      <div className="relative p-4 pb-0">
+        <h1 className="mb-3 text-2xl font-bold text-[#C3F32C]">
+          <span className="shine-text">{barbershop.name}</span>{" "}
+        </h1>
 
         <div className="mb-2 flex items-center gap-1">
-          <MapIcon className="h-4 w-4 text-[#254F50] size={18}" />
+          <MapIcon className="size={18} h-4 w-4 text-[#254F50]" />
           <p className="text-sm text-white">{barbershop.address}</p>
         </div>
-        {/* Exibir endereço da barbearia */} 
+        {/* Exibir endereço da barbearia */}
         <div className="mb-2 flex items-center gap-1">
-          <StarIcon className="h-4 w-4 text-[#254F50] fill-[#254F50]"  />
-          <p className=" text-sm text-white">4,8 (899 avaliações)</p>
+          <StarIcon className="h-4 w-4 fill-[#254F50] text-[#254F50]" />
+          <p className="text-sm text-white">4,8 (899 avaliações)</p>
         </div>
-
       </div>
+
       {/* Exibir descrição da barbearia */}
-      <div className="p-4 rounded-lg mb-4  ">
-       
-        <Card className=" border-none p-4 mb-4">
-           <h2 className="uppercase text-[#C3F32C] pl-1 pb-0" >Sobre nós. . .</h2>
-          <p className="text-[13px] font-[Arial] text-white">{barbershop.description}</p>
+      <div className="mb-4 rounded-lg p-4 pb-0">
+        <Card className="mb-4 border-none p-4">
+          <div className="flex items-center gap-1 h-4">
+            <h2 className="pl-1 text-[#C3F32C] uppercase m-0 pt-1">Sobre nós</h2>
+             <User className="text-[#C3F32C] h-5 w-5" />
+          </div>
+          <p className="text-[15px] text-white ">
+            {barbershop.description}
+          </p>
         </Card>
       </div>
-      
-     <div className="p-5">
-        <h2 className="text-xs font-bold uppercase">Serviços</h2>
+
+      {/* Banner Image */}
+      <div className="p-4 pb-0">
+        <div className="relative h-37.5 w-full overflow-hidden rounded-xl">
+          <img
+            src="/maoBannerSite.png"
+            alt="img"
+            className="h-full w-full object-cover"
+          />
+        </div>
+      </div>
+
+      <div className="p-5">
+        <h2 className="text-xs font-bold uppercase p-3">Serviços</h2>
         {barbershop.services.map((service) => (
           <ServiceItem key={service.id} service={service} />
         ))}
-     </div>
-
+      </div>
     </div>
   )
 }
