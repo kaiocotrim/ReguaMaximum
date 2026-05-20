ff46ef0069a88bedb98ffce472fd7f145d070849
kaiocotrim <kaioalan40@gmail.com>
2026-05-20 11:53:08 -0300

    feat: foiii

--- barber-cloud/app/_lib/prisma.ts
+++ barber-cloud/app/_lib/prisma.ts
@@ -1,19 +1,44 @@
-import { PrismaPg } from "@prisma/adapter-pg"
+// import { PrismaPg } from "@prisma/adapter-pg"
+
+
+// import { PrismaClient } from "../generated/prisma/client"
+
+// const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+
+// const databaseUrl = process.env.DATABASE_URL
+
+// if (!databaseUrl) {
+// 	throw new Error("DATABASE_URL nao esta definida")
+// }
+
+// export const db =
+// 	globalForPrisma.prisma ||
+// 	new PrismaClient({
+// 		adapter: new PrismaPg(databaseUrl),
+// 	})
+
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db
+
+// import { PrismaClient } from "../generated/prisma/client"
+
+// const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+
+// export const db =
+//   globalForPrisma.prisma || new PrismaClient()
+
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db
+
 
 import { PrismaClient } from "../generated/prisma/client"
+import { PrismaNeon } from "@prisma/adapter-neon"
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
-const databaseUrl = process.env.DATABASE_URL
-
-if (!databaseUrl) {
-	throw new Error("DATABASE_URL nao esta definida")
-}
+const adapter = new PrismaNeon({ 
+  connectionString: process.env.DATABASE_URL! 
+})
 
 export const db =
-	globalForPrisma.prisma ||
-	new PrismaClient({
-		adapter: new PrismaPg(databaseUrl),
-	})
+  globalForPrisma.prisma || new PrismaClient({ adapter })
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db
