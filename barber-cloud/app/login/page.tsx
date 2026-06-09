2971b1b9d766b4b513cfe2c21787aff1b772ba57
kaiocotrim <kaioalan40@gmail.com>
2026-06-09 10:13:12 -0300

    feat: page de reset senha criada

--- barber-cloud/app/login/page.tsx
+++ barber-cloud/app/login/page.tsx
@@ -5,7 +5,6 @@ export default function Page() {
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10 bg-[#121212]">
         <LoginForm />
-        
     </div>
   )
 }
