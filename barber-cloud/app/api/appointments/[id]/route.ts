0aeb8b5d3ea6258da2be758fd5d4244e1cba6e5b
kaiocotrim <kaioalan40@gmail.com>
2026-06-18 09:24:34 -0300

    feat: rota delete

--- barber-cloud/app/api/appointments/[id]/route.ts
+++ barber-cloud/app/api/appointments/[id]/route.ts
@@ -2,28 +2,28 @@ import { db } from "@/app/_lib/prisma"
 import { NextResponse } from "next/server"
 
 // Manipulador de requisição DELETE
+
 export async function DELETE(
   request: Request,
-  context: { params: { id: string } } // Usa-se `context` explicitamente
+  { params }: { params: Promise<{ id: string }> }
 ) {
-  const { id } = context.params;  // Resolve `params` corretamente
+  const { id } = await params;
 
   try {
-    await db.booking.delete({
+    await db.Booking.delete({
       where: {
         id,
       },
-    })
+    });
 
     return NextResponse.json(
-      { message: "Agendamento cancelado com sucesso!" },
+      { message: "Agendamento cancelado com sucesso." },
       { status: 200 }
-    )
+    );
   } catch (error) {
-    console.error("Erro ao cancelar consulta:", error);
     return NextResponse.json(
-      { error: "Erro ao cancelar agendamento" },
+      { message: "Erro ao cancelar o agendamento." },
       { status: 500 }
-    )
+    );
   }
 }
