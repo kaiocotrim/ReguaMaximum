acdad3ebcfa9ac812f452864e02ede4e65dbe633
kaiocotrim <kaioalan40@gmail.com>
2026-07-16 10:38:45 -0300

    feat: att do compoenten

--- barber-cloud/app/_components/dashboardComponents/agendamentos/total/Agendados.tsx
+++ barber-cloud/app/_components/dashboardComponents/agendamentos/total/Agendados.tsx
@@ -1,4 +1,5 @@
-import { db } from "@/app/_lib/prisma";
+import { Card } from "@/app/_components/ui/card"
+import { db } from "@/app/_lib/prisma"
 
 export default async function Agendados() {
   const appointments = await db.booking.findMany({
@@ -11,23 +12,26 @@ export default async function Agendados() {
     orderBy: {
       date: "asc",
     },
-  });
+  })
 
   return (
-    <>
-      {appointments.map((appointment) => (
-        <div key={appointment.id}>
-          <h2>{appointment.user.name}</h2>
+    <Card className="mt-5">
+      
+        {appointments.map((appointment) => (
+          <div key={appointment.id} className="">
+            <p>Cliente:</p>
+            <h2>{appointment.user.name}</h2>
+            <p>Barbeiro</p>
+            <p>{appointment.barber.nome}</p>
+            <p>Serviço</p>
+            <p>{appointment.service.name}</p>
+            <p>Barbearia</p>
+            <p>{appointment.barbershop.name}</p>
+            <p>Data:</p>
+            <p>{appointment.date.toLocaleString()}</p>
+          </div>
+        ))}
 
-          <p>{appointment.barber.nome}</p>
-
-          <p>{appointment.service.name}</p>
-
-          <p>{appointment.barbershop.name}</p>
-
-          <p>{appointment.date.toLocaleString()}</p>
-        </div>
-      ))}
-    </>
-  );
+    </Card>
+  )
 }
