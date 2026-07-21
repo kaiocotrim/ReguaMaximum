d902fabe474768f6b2cff623f8ffd759939a97cd
kaiocotrim <kaioalan40@gmail.com>
2026-07-21 12:02:27 -0300

    feat: tempo em minutos att

--- barber-cloud/app/_actions/service/create-service.ts
+++ barber-cloud/app/_actions/service/create-service.ts
@@ -1,26 +1,26 @@
-"use server";
+"use server"
 
-import { db } from "@/app/_lib/prisma";
-import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-import { redirect } from "next/navigation";
-import { revalidatePath } from "next/cache";
+import { db } from "@/app/_lib/prisma"
+import { getServerSession } from "next-auth/next"
+import { authOptions } from "@/app/api/auth/[...nextauth]/route"
+import { redirect } from "next/navigation"
+import { revalidatePath } from "next/cache"
 
 export async function createService(formData: FormData) {
-  const session = await getServerSession(authOptions);
+  const session = await getServerSession(authOptions)
 
   if (!session?.user) {
-    throw new Error("Usuário não autenticado.");
+    throw new Error("Usuário não autenticado.")
   }
 
   const barbershop = await db.barbershop.findFirst({
     where: {
       ownerId: (session.user as any).id,
     },
-  });
+  })
 
   if (!barbershop) {
-    throw new Error("Barbearia não encontrada.");
+    throw new Error("Barbearia não encontrada.")
   }
 
   await db.barbeshopService.create({
@@ -28,12 +28,13 @@ export async function createService(formData: FormData) {
       name: formData.get("name") as string,
       description: formData.get("description") as string,
       price: Number(formData.get("price")),
-      imageUrl: "", // por enquanto
+      duration: Number(formData.get("duration")),
+      imageUrl: "",
       barbershopId: barbershop.id,
     },
-  });
+  })
 
-  revalidatePath("/dashboard/servicos");
+  revalidatePath("/dashboard/servicos")
 
-  redirect("/dashboard/servicos");
+  redirect("/dashboard/servicos")
 }
