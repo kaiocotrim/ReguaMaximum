e6511ec0bb129e7e357a7904ee6fc013430b3827
kaiocotrim <kaioalan40@gmail.com>
2026-08-05 14:41:22 -0300

    feat: subiu tudo para o mobile

---
+++ ReguaMaximaApp/app/_layout.tsx
@@ -0,0 +1,19 @@
+import "../global.css";
+
+import { Stack } from "expo-router";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+export default function RootLayout() {
+  return (
+    <SafeAreaProvider>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          contentStyle: {
+            backgroundColor: "#000000",
+          },
+        }}
+      />
+    </SafeAreaProvider>
+  );
+}
